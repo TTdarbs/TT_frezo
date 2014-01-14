edc14ae1bbc4d00b6baa60598d16bdd683af1021
HSmelens <hgx0303@gmail.com>
2014-01-14 23:43:20 +0200

    5

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -44,7 +44,7 @@
 		</div>
 		<div class="row">
 			<div class="col-md-4">
-				<h2>Get Started1</h2>
+				<h2>Get Started4</h2>
 				<p>The controller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
 				<p>This view can be found at <code>APPPATH/views/welcome/index.php</code>.</p>
 				<p>You can modify these files to get your application started quickly.</p>
