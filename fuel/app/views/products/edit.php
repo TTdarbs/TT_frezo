f0efdc3bc5d9faaba50a1c8d7cc60ccb35cb5dd3
Ivars <ivars.stinkevics@gmail.com>
2014-01-16 19:57:53 +0200

    var pievienot tikai admins jaunumus un produktus

--- fuel/app/views/products/edit.php
+++ fuel/app/views/products/edit.php
@@ -1,7 +1,15 @@
 <h2>Editing <span class='muted'>Product</span></h2>
 <br>
 
+<?php if (Auth::has_access("product.edit")): ?>
+
 <?php echo render('products/_form'); ?>
+
 <p>
-	<?php echo Html::anchor('products/view/'.$product->id, 'View'); ?> |
-	<?php echo Html::anchor('products', 'Back'); ?></p>
+	<?php echo Html::anchor('products/view/'.$product->id, 'View'); ?>
+	</p>
+
+
+<?php endif; ?>
+
+<p> <?php echo Html::anchor('products', 'Back'); ?></p>
