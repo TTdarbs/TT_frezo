d9a53f6d0a63473d6c319643c6be79200934c3ed
Ivars <ivars.stinkevics@gmail.com>
2014-01-16 19:58:07 +0200

    var pievienot tikai admins jaunumus un produktus

--- fuel/app/views/news/edit.php
+++ fuel/app/views/news/edit.php
@@ -1,7 +1,36 @@
 <h2>Editing <span class='muted'>News</span></h2>
 <br>
 
+<?php if (Auth::has_access("news.edit")): ?>
+
 <?php echo render('news/_form'); ?>
+
 <p>
-	<?php echo Html::anchor('news/view/'.$news->id, 'View'); ?> |
-	<?php echo Html::anchor('news', 'Back'); ?></p>
+	<?php echo Html::anchor('news/view/'.$news->id, 'View'); ?>
+</p>
+	
+
+<?php endif; ?>
+
+
+<p> <?php echo Html::anchor('news', 'Back'); ?></p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
